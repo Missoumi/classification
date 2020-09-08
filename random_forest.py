57fc701625f2abf1144cc5a686763e8f55138e2e
MISSOUMI Abdellatif <44708954+Missoumi@users.noreply.github.com>
2020-09-08 16:14:54 +0100

    first commit

---
+++ random_forest.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+
+# Load dataset
+dataset = pd.read_csv("./P14-Part3-Classification/Section 19 - Kernel SVM/Python/Social_Network_Ads.csv")
+
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, -1].values
+
+
+# Split the dataset
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+
+# Feature scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Fitting logistic regression
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators=300, criterion="gini")
+classifier.fit(X_train, y_train)
+
+# Predict on test set
+y_pred = classifier.predict(X_test)
+
+# Confusion matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+# accuracy metrics
+print("Accuracy :", accuracy_score(y_test, y_pred))
+
+
